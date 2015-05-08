b8b53904219fcd38bf12d117e90e392ac7372e4d
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-05-08 11:40:35 -0700

    finished watching vid 6

--- index.php
+++ index.php
@@ -5,18 +5,18 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //make Constants using define 
-define('clientID', '6adf1fecc34bac4847a5085fc40cf19');
+define('clientID', '6adf1fecc34b4ac4847a5085fc40cf19');
 define('clientSecret', 'f927f361c731457f97e3cc97e4e25b37');
 define('redirectURI', 'http://localhost/Audiopunkpictures/index.php');
 define('ImageDirectory', 'pics/');
 //isset checks for booleans
-if isset(($_GET['code'])) {
+if (isset($_GET['code'])) {
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID,
-									'client_secret' => clientSecert,
+									'client_secret' => clientSecret,
 									'grant_type' => 'authorization_code',
-									'redirect_uri' = >redirectURI,
+									'redirect_uri' => redirectURI,
 									'code' => $code,
 									);
 
